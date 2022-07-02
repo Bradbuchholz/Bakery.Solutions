4c8cf9408f70e3c9fb2d67e681ff236b55397dc0
Bradbuchholz <bradbuchholz@gmail.com>
2022-07-02 08:57:55 -0700

    update Bread.cs logic

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,3 +1,31 @@
 using System;
 using System.Collections.Generic;
 
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    public int BreadOrder { get; set; }
+
+    public Bread(int order)
+    {
+      BreadOrder = order;
+    }
+    public int OrderTotal()
+    {
+      if (BreadOrder % 3 == 0)
+      {
+        int totalPrice = (BreadOrder - (BreadOrder / 3)) * 5;
+        return totalPrice;
+      }
+      else if (BreadOrder >= 25)
+      {
+        return BreadOrder * 1;
+      }
+      else
+      {
+        return BreadOrder * 5;
+      }
+    }
+  }
+}
