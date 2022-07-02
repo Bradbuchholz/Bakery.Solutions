11fad00d3f7ddbef7c3ef7659104373b04226724
Bradbuchholz <bradbuchholz@gmail.com>
2022-07-01 17:07:44 -0700

    tests written

--- Bakery.Test/ModelTests/BakeryTests.cs
+++ Bakery.Test/ModelTests/BakeryTests.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Bakery.Models;
+using System; 
 
-namespace Bakery.Test
+namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
@@ -9,8 +11,55 @@ namespace Bakery.Test
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread(5);
-      Assert.AreEqual(typeof(Bread), newBread.GetType());
+        Bread newBread = new Bread(5);
+        Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void OrderCost_GetsOrderDealTotal_Int()
+    {
+      int breadOrder = 6;
+      Bread newOrder = new Bread(breadOrder);
+      int result = newOrder.OrderTotal();
+      Assert.AreEqual(20, result);
+    }
+
+    [TestMethod]
+    public void OrderCost_GetsNoDealOrderTotal_Int()
+    {
+      int breadOrder = 1;
+      Bread newOrder = new Bread(breadOrder);
+      int result = newOrder.OrderTotal();
+      Assert.AreEqual( 5, result);
+    }
+
+    [TestClass]
+    public class PastryTests
+    {
+      [TestMethod]
+      public void PastyConstructor_CreatesInstanceOfPastry_Pasty()
+      {
+        Pastry newPastry = new Pastry(4);
+        Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+      }
+
+      [TestMethod]
+      public void OrderCost_GetsPastryTotal_Int()
+      {
+        int pastryOrder = 1;
+        Pastry newOrder = new Pastry(pastryOrder);
+        int result  = newOrder.OrderTotal();
+        Assert.AreEqual( 2, result);
+        }
+
+      [TestMethod]
+      public void OrderCost_GetsPastryDealTotal_Int()
+      {
+        int pastryOrder = 3;
+        Pastry newOrder = new Pastry(pastryOrder);
+        int result  = newOrder.OrderTotal();
+        Assert.AreEqual( 5, result);
+      }
     }
   }
-}
+};
---
+++ Bakery/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using Bakery.Models;
+
+
+public class Customer
+{
+  static void Main()
+  {
+    Console.WriteLine("Welcome to Pierre's Bakery!");
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("We have bread and pastries for sale today.");
+    Console.WriteLine("Today our bread is $5 or you can buy 2 and get 1 free!");
+    Console.WriteLine("And for today only,if you order 25 loaves or more they are only $1 each! Wow!");
+    Console.WriteLine("Our pastries today are $2, or 3 for $5.");
+    Console.WriteLine("How many loaves of bread can I get you today?");
+    int loaves = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{loaves} loaves eh? How many delicious pastries can I add to your order today?");
+    int sweets = int.Parse(Console.ReadLine());
+
+    Bread loafOrder = new Bread(loaves);
+    Pastry sweetOrder = new Pastry(sweets);
+
+    Console.WriteLine($"Ok that's {loaves} loaves of bread, and {sweets} pastries");
+    Console.WriteLine($"Your order total is ${loafOrder.OrderTotal() + sweetOrder.OrderTotal()}");
+  }
+}
